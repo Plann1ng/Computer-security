17f343b2b079fdaba717434ac350aed4d9079885
Ryustem Shaban <rstmduran@gmail.com>
2023-01-22 23:29:18 +0100

    Started with Caeser and columnar cypher implementation.

---
+++ Caesar-Columnar.py
@@ -0,0 +1,32 @@
+import os
+import math
+
+# Read the file
+
+
+def reader(string):
+    with open(string, mode="r", encoding="utf-8") as file:
+        file = file.read().split()
+        return file
+
+# Encryption Caeser cipher
+# Did not managed to use "   '  " since it get's mixed with the string
+
+
+def enc_ceasar(key, plain):
+    # reading the file inside the function
+    text = reader(plain)
+    # Alphabet to take the shifted index and some other additional characters
+    alpha = 'abcdefghjiklmnopqrstuvwxyz,.:*-()" '
+    output = ''
+    for word in text:
+        for letter in word:
+            # Check the index of the current letter
+            letter = letter.lower()
+            index = alpha.find(letter)
+            # Use the key for the cipher index
+            cypherpos = (index+key) % 34
+            # Append to empty string
+            output += alpha[cypherpos]
+        output += " "
+    return output
